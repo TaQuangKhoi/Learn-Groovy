4328704761b70259fd21776f20af0fc5c78c75e5
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2023-11-02 08:13:30 +0000

    Test DateTimeFormatter

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,3 +1,13 @@
+import java.time.LocalDateTime
+
 static void main(String[] args) {
-  println "Hello world!"
+  // 2023-10-03T17:00 turn this into LocalDateTime
+  def startDate = LocalDateTime.parse("2023-10-03T17:00")
+
+  def dateFormatter = java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+  def startDateFormatted = startDate != null ? startDate.format(dateFormatter) : ""
+
+  println startDate.toString()
+  println startDateFormatted
 }
