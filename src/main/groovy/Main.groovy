e135c81caea22502409d659c83f6d92b690e33ef
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-04 22:05:36 +0700

    Try format OffsetDateTime

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,10 +1,28 @@
 import java.time.LocalDateTime
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
 
 static void main(String[] args) {
+  formatOffsetDateTime()
+}
+
+def formatOffsetDateTime() {
+    // 2023-10-03T17:00+08:00 turn this into OffsetDateTime
+    def startDate = OffsetDateTime.parse("2023-10-07T23:53:39Z")
+
+    def dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+
+    def startDateFormatted = startDate != null ? startDate.format(dateFormatter) : ""
+
+    println startDate.toString()
+    println startDateFormatted
+}
+
+def formatLocalDateTime() {
   // 2023-10-03T17:00 turn this into LocalDateTime
   def startDate = LocalDateTime.parse("2023-10-03T17:00")
 
-  def dateFormatter = java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd")
+  def dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
   def startDateFormatted = startDate != null ? startDate.format(dateFormatter) : ""
 
